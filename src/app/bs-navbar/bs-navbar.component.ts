bbde0ecf5bd68ece2f6f08528114a6223a3c87d3
Ashish kumar shrivastav <a.shrivastav@globallogic.com>
2018-08-16 10:55:27 +0530

    Using the async pipe

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase';
+// When working with firebase, we should always unsubscribe to the observables.
+// One way is to implement ngOnDestroy
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent implements OnInit {
-
+  user$: Observable<firebase.User>;
   constructor(private afAuth: AngularFireAuth) {
-    this.afAuth.authState.subscribe(x => console.log(x));
+    this.user$ = this.afAuth.authState;
   }
 
   ngOnInit() {
